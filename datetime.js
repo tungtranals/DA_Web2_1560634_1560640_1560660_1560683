7061f84a2dfe46204582f00e60aeacb95582a1c9
1560640 <1560640@student.hcmus.edu.vn>
2018-06-27 22:38:54 +0700

    test datetime

--- datetime.js
+++ datetime.js
@@ -9,15 +9,3 @@ var t3 = date.getDate();
 var t1 = date.getMonth();
 var t2 = date.getFullYear();
 var h = date.getHours();
-
-var m = date.getMinutes();
-var s = date.getSeconds();
-console.log(date);
-var StrDate = t1 + "/" + (t3+1) + "/" + t2 + "  " + h + ":" + m + ":" + s;
-console.log(StrDate);
-var endDatess = Date.parse(StrDate.toString());
-console.log(endDatess+"/"+endDate);
-
-/*console.log(endDatess);
-console.log(endDate);
-  console.log((startDate - endDate)/1000 ); */
