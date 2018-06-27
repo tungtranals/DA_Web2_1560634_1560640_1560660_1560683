6a43c025d21d27048757d763b2204ec689a9c746
1560640 <1560640@student.hcmus.edu.vn>
2018-06-27 22:57:37 +0700

    commnet

--- socket_uphinh.js
+++ socket_uphinh.js
@@ -15,23 +15,7 @@ server.listen(3000,function(){
     console.log('server is listening in port 3000')
 });
 
-io.on("connection", function(socket){
-    console.log("ket noi "+socket.io);
-    socket.on("disconnect", function(){
-        console.log(socket.io +"ngat ket noi ");
-    });
-    socket.on("clientsentdata", function(data){
-        console.log(data);
-        io.sockets.emit("senddata",data+" hihi");
-    });
-    var time = 1;
-    setInterval(function() {
-        io.sockets.emit("senddata",time+" hihi");
-        time++;
-    }, 1000);
-});
-
-/*app.get('/html',function(req,res){
+app.get('/html',function(req,res){
     res.sendFile(path.join(__dirname+'/views/senfile.html'));
-});*/
+});
 
