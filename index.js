c63acc067d873b01541ab5e6fe573b4f09f84e47
VinPhamhcmus <39879873+VinPhamhcmus@users.noreply.github.com>
2018-06-28 21:55:14 +0700

    hihi

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 
 //config postgres local
-/*
+
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000, function () { console.log('server is listening in port 3000') });
@@ -35,9 +35,9 @@ var config = {
     idleTimeoutMillis: 30000, // close idle clients after 30 second
 };
 
-*/
-//config heroku postgres database
 
+//config heroku postgres database
+/*
 var http = require('http').createServer(app);
 var io = require("socket.io")(http);
 http.listen(process.env.PORT);
@@ -51,7 +51,7 @@ var config = {
     max: 10, // set pool max size to 20
     idleTimeoutMillis: 30000, // close idle clients after 30 second
 };
-
+*/
 
 
 var pool = new pg.Pool(config)
